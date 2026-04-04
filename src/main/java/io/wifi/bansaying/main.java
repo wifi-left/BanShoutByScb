e51d5782e03c7964088968feca0153bd62c3f1a9
wifi-left <ysy1679682327@outlook.com>
2026-04-05 00:25:06 +0800

    support 26.1.1

--- src/main/java/io/wifi/bansaying/main.java
+++ src/main/java/io/wifi/bansaying/main.java
@@ -1,64 +1,60 @@
 package io.wifi.bansaying;
-// MyMod.java
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.message.v1.ServerMessageEvents;
-import net.minecraft.command.permission.Permission;
-import net.minecraft.command.permission.PermissionLevel;
-import net.minecraft.network.message.SignedMessage;
-import net.minecraft.scoreboard.AbstractTeam;
-import net.minecraft.scoreboard.ScoreboardCriterion;
-import net.minecraft.scoreboard.ScoreboardObjective;
-import net.minecraft.scoreboard.Team;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-
-import static net.minecraft.server.command.CommandManager.literal;
+import net.minecraft.ChatFormatting;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.permissions.Permission;
+import net.minecraft.server.permissions.PermissionLevel;
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.scores.PlayerTeam;
+import net.minecraft.world.scores.ScoreHolder;
+import net.minecraft.world.scores.criteria.ObjectiveCriteria;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
-import static net.minecraft.server.command.CommandManager.argument;
 
 public class main implements ModInitializer {
     // 定义一个键绑定
     // ## 0 for nothing; 1 ban other team; 2 ban own team; 3 all banned; 4 ban
     // 命令：/tshout
-    public static Permission perm_2 = new Permission.Level(PermissionLevel.GAMEMASTERS); // 2
+    public static final Permission perm_2 = new Permission.HasCommandLevel(PermissionLevel.GAMEMASTERS);
 
-    public static ScoreboardObjective ModObj = null;
+    public static Objective ModObj = null;
     public static Logger LOGGER = LoggerFactory.getLogger("MuteByCmd");
 
     private static void registerCommands() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(literal("sshout").executes(context -> {
+            dispatcher.register(Commands.literal("sshout").executes(context -> {
                 context.getSource()
-                        .sendFeedback(() -> Text.literal("Usage: /sshout <Contents>").formatted(Formatting.RED), false);
+                        .sendSuccess(() -> Component.literal("Usage: /sshout <Contents>").withStyle(ChatFormatting.RED), false);
                 return 1;
-            }).then(argument("content", StringArgumentType.greedyString()).executes((ctx) -> {
+            }).then(Commands.argument("content", StringArgumentType.greedyString()).executes((ctx) -> {
                 String content = StringArgumentType.getString(ctx, "content");
-                ServerCommandSource source = ctx.getSource();
-                if (source.getPermissions().hasPermission(perm_2) || source.getPlayer() == null) {
+                var source = ctx.getSource();
+                if (source.permissions().hasPermission(perm_2) || source.getPlayer() == null) {
                     sendChatMessageToAll(source, content, true);
                 } else {
 
-                    ServerPlayerEntity player = source.getPlayer();
-                    AbstractTeam team = player.getScoreboardTeam();
+                    ServerPlayer player = source.getPlayer();
+                    PlayerTeam team = player.getTeam();
                     String playerTeam = "#";
                     if (team != null) {
                         playerTeam = team.getName();
                     }
                     int chatType = 0;
                     try {
-                        chatType = player.getEntityWorld().getScoreboard()
-                                .getScore(new MixScoreHolder(playerTeam), ModObj)
-                                .getScore();
+                        chatType = player.level().getScoreboard()
+                                .getPlayerScoreInfo(ScoreHolder.forNameOnly(playerTeam), ModObj).value();
                     } catch (Exception e) {
                         chatType = 0;
                     }
@@ -66,9 +62,8 @@ public class main implements ModInitializer {
 
                     if ((chatType & 4) != 0) {
                         // 禁止 shout
-                        source.sendFeedback(
-                                () -> Text.literal("You cannot shout right now.").formatted(Formatting.RED),
-                                false);
+                        source.sendFailure(
+                                Component.literal("You cannot shout right now.").withStyle(ChatFormatting.RED));
                         return 1;
                     }
                     sendChatMessageToAll(source, content);
@@ -81,25 +76,25 @@ public class main implements ModInitializer {
 
     }
 
-    private static void sendChatMessageToAll(ServerCommandSource source, String content) {
+    private static void sendChatMessageToAll(CommandSourceStack  source, String content) {
         sendChatMessageToAll(source, content, false);
     }
 
-    private static void sendChatMessageToAll(ServerCommandSource source, String content, boolean op) {
-        ServerPlayerEntity player = source.getPlayer();
-        MutableText text = Text.literal("");
+    private static void sendChatMessageToAll(CommandSourceStack  source, String content, boolean op) {
+        ServerPlayer player = source.getPlayer();
+        MutableComponent text = Component.literal("");
         if (player == null) {
-            text = text.append(Text.literal("[SHOUT][OP] ").formatted(Formatting.GOLD))
-                    .append(Text.literal("CONSOLES").formatted(Formatting.GREEN))
-                    .append(Text.literal(": ").formatted(Formatting.GRAY))
-                    .append(Text.literal(content).formatted(Formatting.WHITE));
+            text = (Component.literal("[SHOUT][OP] ").withStyle(ChatFormatting.GOLD))
+                    .append(Component.literal("CONSOLES").withStyle(ChatFormatting.GREEN))
+                    .append(Component.literal(": ").withStyle(ChatFormatting.GRAY))
+                    .append(Component.literal(content).withStyle(ChatFormatting.WHITE));
         } else {
-            text = text.append(Text.literal("[SHOUT]" + (op ? "[OP]" : "") + " ").formatted(Formatting.GOLD))
+            text = (Component.literal("[SHOUT]" + (op ? "[OP]" : "") + " ").withStyle(ChatFormatting.GOLD))
                     .append(player.getDisplayName())
-                    .append(Text.literal(": ").formatted(Formatting.GRAY))
-                    .append(Text.literal(content).formatted(Formatting.WHITE));
+                    .append(Component.literal(": ").withStyle(ChatFormatting.GRAY))
+                    .append(Component.literal(content).withStyle(ChatFormatting.WHITE));
         }
-        source.getServer().getPlayerManager().broadcast(text, false);
+        source.getServer().getPlayerList().broadcastSystemMessage(text, false);
     }
 
     @Override
@@ -108,12 +103,12 @@ public class main implements ModInitializer {
         registerCommands();
         ServerLifecycleEvents.SERVER_STARTED.register((server) -> {
             try {
-                ModObj = server.getScoreboard().addObjective("BAMBOO_MOD_SAYING", ScoreboardCriterion.DUMMY,
-                        Text.literal("[*MOD: Scoreboard Saying Control]"), ScoreboardCriterion.RenderType.INTEGER,
+                ModObj = server.getScoreboard().addObjective("BAMBOO_MOD_SAYING", ObjectiveCriteria.DUMMY,
+                        Component.literal("[*MOD: Scoreboard Saying Control]"), ObjectiveCriteria.RenderType.INTEGER,
                         false, null);
             } catch (IllegalArgumentException e) {
                 // 已存在
-                ModObj = server.getScoreboard().getNullableObjective("BAMBOO_MOD_SAYING");
+                ModObj = server.getScoreboard().getObjective("BAMBOO_MOD_SAYING");
                 if (ModObj == null) {
                     LOGGER.error("Can't create or find objective called 'BAMBOO_MOD_SAYING'!");
                 }
@@ -122,15 +117,15 @@ public class main implements ModInitializer {
         ServerMessageEvents.ALLOW_CHAT_MESSAGE.register((message, sender, params) -> {
             // System.out.println(message.getSignedContent()+" by
             // "+sender.getName().getString());
-            AbstractTeam team = sender.getScoreboardTeam();
+            PlayerTeam team = sender.getTeam();
             String playerTeam = "#";
             if (team != null) {
                 playerTeam = team.getName();
             }
             int chatType = 0;
             try {
-                chatType = sender.getEntityWorld().getScoreboard().getScore(new MixScoreHolder(playerTeam), ModObj)
-                        .getScore();
+                chatType = sender.level().getScoreboard().getPlayerScoreInfo( ScoreHolder.forNameOnly(playerTeam), ModObj)
+                        .value();
             } catch (Exception e) {
                 chatType = 0;
             }
@@ -140,67 +135,67 @@ public class main implements ModInitializer {
                 return true;
             } else {
                 if (chatType == 3) {
-                    sender.sendMessage(
-                            Text.literal("You cannot speak right now. \nTry command instead: /sshout <Content>")
-                                    .formatted(Formatting.RED));
+                    sender.sendSystemMessage(
+                            Component.literal("You cannot speak right now. \nTry command instead: /sshout <Content>")
+                                    .withStyle(ChatFormatting.RED));
                 } else if (chatType == 2 || chatType == 6) {
                     LOGGER.info(
-                            "[TEAM_ONLY] " + sender.getDisplayName().getString() + ": " + message.getSignedContent());
+                            "[TEAM_ONLY] " + sender.getDisplayName().getString() + ": " + message.signedContent());
                     sendMessageToOwnTeam(message, sender, playerTeam);
                 } else if (chatType == 1 || chatType == 5) {
                     if (team == null) {
-                        sendMessageToOtherTeam(message, sender, playerTeam, Text.literal("NORMAL"));
+                        sendMessageToOtherTeam(message, sender, playerTeam, Component.literal("NORMAL"));
                         LOGGER.info("[IN_TEAM_ONLY] " + sender.getDisplayName().getString() + ": "
-                                + message.getSignedContent());
+                                + message.signedContent());
                     } else {
-                        Team team1 = (Team) team;
+                        PlayerTeam team1 = team;
                         sendMessageToOtherTeam(message, sender, playerTeam, team1.getDisplayName());
                         LOGGER.info("[OTHER_TEAM_ONLY] " + sender.getDisplayName().getString() + ": "
-                                + message.getSignedContent());
+                                + message.signedContent());
 
                     }
 
                 } else if (chatType == 7) {
-                    sender.sendMessage(Text.literal("You cannot speak or shout right now.").formatted(Formatting.RED));
+                    sender.sendSystemMessage(Component.literal("You cannot speak or shout right now.").withStyle(ChatFormatting.RED));
                 }
                 return false;
             }
         });
     }
 
-    private void sendMessageToOtherTeam(SignedMessage message, ServerPlayerEntity sender, String playerTeam,
-            Text teamDisplay) {
-        MutableText raw_message = Text.literal("").formatted(Formatting.WHITE)
-                .append(Text.literal("[").formatted(Formatting.GRAY).append(teamDisplay)
-                        .formatted(Formatting.WHITE).append("]").formatted(Formatting.GRAY))
-                .append(Text.literal(" "))
-                .append(sender.getDisplayName()).append(Text.literal(": ").formatted(Formatting.GRAY))
-                .append(((MutableText) message.getContent()).formatted(Formatting.WHITE));
-        sender.sendMessage(raw_message.append(Text.literal("\nThis message can't be viewed by your teammates!")
-                .formatted(Formatting.DARK_GRAY).formatted(Formatting.ITALIC)));
-        sender.getEntityWorld().getServer().getPlayerManager().getPlayerList().forEach((player) -> {
-            Team scoreTeam = player.getScoreboardTeam();
+    private void sendMessageToOtherTeam(PlayerChatMessage message, ServerPlayer sender, String playerTeam,
+            Component teamDisplay) {
+        MutableComponent raw_message = Component.literal("").withStyle(ChatFormatting.WHITE)
+                .append(Component.literal("[").withStyle(ChatFormatting.GRAY).append(teamDisplay)
+                        .withStyle(ChatFormatting.WHITE).append("]").withStyle(ChatFormatting.GRAY))
+                .append(Component.literal(" "))
+                .append(sender.getDisplayName()).append(Component.literal(": ").withStyle(ChatFormatting.GRAY))
+                .append(((MutableComponent) message.decoratedContent()).withStyle(ChatFormatting.WHITE));
+        sender.sendSystemMessage(raw_message.append(Component.literal("\nThis message can't be viewed by your teammates!")
+                .withStyle(ChatFormatting.DARK_GRAY).withStyle(ChatFormatting.ITALIC)));
+        sender.level().getServer().getPlayerList().getPlayers().forEach((player) -> {
+            PlayerTeam scoreTeam = player.getTeam();
             String teamName = "#";
             if (scoreTeam != null)
-                teamName = player.getScoreboardTeam().getName();
+                teamName = scoreTeam.getName();
             if (teamName != playerTeam) {
-                player.sendMessage(raw_message, false);
+                player.sendSystemMessage(raw_message, false);
             }
         });
     }
 
-    private void sendMessageToOwnTeam(SignedMessage message, ServerPlayerEntity sender, String playerTeam) {
-        Text raw_message = Text.literal("").formatted(Formatting.WHITE)
-                .append(Text.literal("[TEAM]").formatted(Formatting.GRAY)).append(Text.literal(" "))
-                .append(sender.getDisplayName()).append(Text.literal(": ").formatted(Formatting.GRAY))
-                .append(((MutableText) message.getContent()).formatted(Formatting.WHITE));
-        sender.getEntityWorld().getServer().getPlayerManager().getPlayerList().forEach((player) -> {
-            Team scoreTeam = player.getScoreboardTeam();
+    private void sendMessageToOwnTeam(PlayerChatMessage message, ServerPlayer sender, String playerTeam) {
+        Component raw_message = Component.literal("").withStyle(ChatFormatting.WHITE)
+                .append(Component.literal("[TEAM]").withStyle(ChatFormatting.GRAY)).append(Component.literal(" "))
+                .append(sender.getDisplayName()).append(Component.literal(": ").withStyle(ChatFormatting.GRAY))
+                .append((message.decoratedContent().copy()).withStyle(ChatFormatting.WHITE));
+        sender.level().getServer().getPlayerList().getPlayers().forEach((player) -> {
+            PlayerTeam scoreTeam = player.getTeam();
             String teamName = "#";
             if (scoreTeam != null)
-                teamName = player.getScoreboardTeam().getName();
+                teamName = scoreTeam.getName();
             if (teamName == playerTeam) {
-                player.sendMessage(raw_message, false);
+                player.sendSystemMessage(raw_message, false);
             }
         });
     }
